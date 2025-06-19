f38356983aceb3a9d56312e2e4495c141ae0ac58
Nitish Kumar <nitishbhk8@gmail.com>
2025-06-19 11:46:46 +0530

    PYTHON

---
+++ EX-6 Q.2 Print length of a List.py
@@ -0,0 +1,7 @@
+IPLteams= ["RCB", "CSK", "MI", "KKR", "DC", "KXIP", "LSG", "GT", "SRH", "RR" ]
+Players=["Virat", "Dhoni", "Rohit", "Bumrah", "shreyash iyer", "Sachin", "Bhuvneshwar"]
+def print_len(List):
+    print(len(List))
+
+print_len(IPLteams)
+print_len(Players)
