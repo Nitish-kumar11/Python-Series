bebcc89c53a1f9844ca9caf93afd936404ebfa15
Nitish Kumar <nitishbhk8@gmail.com>
2025-06-19 11:56:01 +0530

    PYTHON

---
+++ EX-6 Q.3 Pirnt el of a list in a single line.py
@@ -0,0 +1,11 @@
+IPLteams= ["RCB", "CSK", "MI", "KKR", "DC", "KXIP", "LSG", "GT", "SRH", "RR" ]
+Players=["Virat", "Dhoni", "Rohit", "Bumrah", "shreyash iyer", "Sachin", "Bhuvneshwar"]
+def print_len(List):
+    print(len(List))
+
+def print_List(List):
+    for item in List:
+        print(item, end=" ")
+
+print_List(IPLteams)
+print_List(Players)
