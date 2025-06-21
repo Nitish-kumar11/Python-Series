7e3e05d8f35a1190f48208acc15032b9acb413c2
Nitish Kumar <nitishbhk8@gmail.com>
2025-06-21 13:25:35 +0530

    OOP's in PYTHON

---
+++ OOP's Concept.py
@@ -0,0 +1,9 @@
+class car:
+    color="red"
+    brand="BMW"
+    owner="Nitish"
+
+car1=car()
+print("car color is:", car1.color)
+print("car brand is:", car1.brand)
+print("car owner is:", car1.owner)      
