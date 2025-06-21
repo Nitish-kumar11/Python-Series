cf2476b342858c791c5e429643cdf9177930f6fd
Nitish Kumar <nitishbhk8@gmail.com>
2025-06-21 12:53:37 +0530

    PYTHON

--- Practice.py
+++ Practice.py
@@ -1,9 +1,14 @@
-#Function to Count Duplicates in a List
-from collections import Counter
+#Function to Merge Two Sorted Lists
+def merge_sorted(l1, l2):
+    result = []
+    i = j = 0
+    while i < len(l1) and j < len(l2):
+        if l1[i] < l2[j]:
+            result.append(l1[i])
+            i += 1
+        else:
+            result.append(l2[j])
+            j += 1
+    return result + l1[i:] + l2[j:]
 
-def count_duplicates(lst):
-    freq = Counter(lst)
-    return {k: v for k, v in freq.items() if v > 1}
-
-print(count_duplicates(["a", "b", "a", "c", "b", "b"]))
-
+print(merge_sorted([1, 3, 5], [2, 4, 6]))  # Output: [1, 2, 3, 4, 5, 6]
