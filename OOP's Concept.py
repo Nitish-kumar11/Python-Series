ec671df6ba8626db8d94d288fc557004e3204eab
Nitish Kumar <nitishbhk8@gmail.com>
2025-06-28 12:56:07 +0530

    Constructor

--- OOP's Concept.py
+++ OOP's Concept.py
@@ -1,6 +1,10 @@
 class car:
-   
-    def __init__(self, name, price ):
+     #default constructor
+     def __init__(self):
+        pass
+    
+    #this is parametrized constructor
+     def __init__(self, name, price ):
         self.brand= name
         self.price= price
         print("adding new car..")
@@ -8,4 +12,4 @@ car1= car("BMW", "2.5CR")
 print(car1.brand, car1.price)
 
 car2= car("Audi", "2.7CR")
-print(car2.brand, car2.price)
+print(car2.brand, car2.price) 
