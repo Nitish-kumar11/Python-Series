239b8ebd13a6f848651020c589cf695c962631ad
Nitish Kumar <nitishbhk8@gmail.com>
2025-07-02 09:17:20 +0530

    Practice Questions

--- OOP's Concept.py
+++ OOP's Concept.py
@@ -1,3 +1,5 @@
+###### CONSTRUCTOR IN PYTHON 
+
 class car:
      #default constructor
      def __init__(self):
@@ -13,3 +15,38 @@ print(car1.brand, car1.price)
 
 car2= car("Audi", "2.7CR")
 print(car2.brand, car2.price) 
+
+
+######  class attributes 
+
+class car:
+    made_in = "India"
+
+    # this is a parametrized constructor
+    def __init__(self, name, price):
+        self.brand = name
+        self.price = price
+        print("adding new car..")
+
+car1 = car("BMW", "2.5CR")
+print(car1.brand, car1.price)
+print(car1.made_in)
+
+car2 = car("Audi", "2.7CR")
+print(car2.brand, car2.price)
+
+
+
+
+###### ONE MORE CLASS ATTRIBUTE EXAMPLE 
+
+class student:
+    college_name="Cambridge institute of technology"
+    name="anonymous" #class attribute 
+
+    def __init__(self, name , marks):
+        self.name=name #object attribute 
+        self.marks=marks
+
+S1=student("Nitish kumar", 81) 
+print(S1.name, S1.marks) # here name does't print anonymous because object attribute precedence is always high than class attribute 
