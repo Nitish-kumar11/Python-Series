be172bc07d5689608836e657ae3e7628e1fc4f81
Nitish Kumar <nitishbhk8@gmail.com>
2025-07-04 10:06:54 +0530

    CONCEPTS

--- OOP's Concept .py
+++ OOP's Concept .py
@@ -1,12 +1,11 @@
-
-###### CONSTRUCTOR IN PYTHON 
+######--------CONSTRUCTOR IN PYTHON-------#######
 
 class car:
-     #default constructor
+     # (default constructor)
      def __init__(self):
         pass
     
-    #this is parametrized constructor
+    # (this is parametrized constructor)
      def __init__(self, name, price ):
         self.brand= name
         self.price= price
@@ -15,15 +14,16 @@ car1= car("BMW", "2.5CR")
 print(car1.brand, car1.price)
 
 car2= car("Audi", "2.7CR")
-print(car2.brand, car2.price) 
+print(car2.brand, car2.price)
+print() 
 
 
-######  class attributes 
+######-----class attributes------######
 
 class car:
     made_in = "India"
 
-    # this is a parametrized constructor
+    #(this is a parametrized constructor)
     def __init__(self, name, price):
         self.brand = name
         self.price = price
@@ -35,22 +35,22 @@ print(car1.made_in)
 
 car2 = car("Audi", "2.7CR")
 print(car2.brand, car2.price)
+print()
 
 
 
-
-###### ONE MORE CLASS ATTRIBUTE EXAMPLE 
+######----ONE MORE CLASS ATTRIBUTE EXAMPLE----######
 
 class student:
     college_name="Cambridge institute of technology"
-    name="anonymous" #class attribute 
+    name="anonymous" # (class attribute)
 
     def __init__(self, name , marks):
-        self.name=name #object attribute 
+        self.name=name # (object attribute)
         self.marks=marks
 
 S1=student("Nitish kumar", 81) 
-print(S1.name, S1.marks) # here name does't print anonymous because object attribute > class attribute 
+print(S1.name, S1.marks) # (here name does't print anonymous because object attribute > class attribute)
 print()
 
 
@@ -61,7 +61,6 @@ print()
 ####-----METHODS-------  #####
 
 
-# Define a class
 class Student:
     
     def __init__(self, name, age):
@@ -79,5 +78,21 @@ student2 = Student("Nicky", 22)
 
 
 student1.display_info()
-print()  # for spacing
+print()  # (for spacing)
 student2.display_info()
+print()
+
+
+
+
+
+ #####---static---method
+
+
+class Student:
+    @staticmethod #decorator 
+    def greet():
+        print("Hello,Nitish welcome to the class")
+
+
+Student.greet()
