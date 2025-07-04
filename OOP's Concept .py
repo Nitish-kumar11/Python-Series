4a5168195230e3eab68a19cc83145a99f79b6660
Nitish Kumar <nitishbhk8@gmail.com>
2025-07-04 10:29:56 +0530

    CONCEPTS

--- OOP's Concept .py
+++ OOP's Concept .py
@@ -117,3 +117,28 @@ class Car:
 
 car1=Car()
 car1.start()
+
+
+
+
+
+
+######------Enapsulation-----######
+
+
+
+class Person:
+    def __init__(self, name):
+        self.__name = name 
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+p = Person("Nitish")
+print(p.get_name()) 
+
+p.set_name("Shradha")
+print(p.get_name()) 
